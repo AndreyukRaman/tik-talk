af1485ceda881fa24f39b803b1c819757e3134a1
Raman Andreyuk <romaandreyuk@gmail.com>
2026-07-15 12:05:07 +0200

    login form with FormBuilder. [formGroup],(ngSubmit), formControlName

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
-export class LoginPage {}
+export class LoginPage {
+  private fb = inject(FormBuilder);
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    if (this.form.invalid) return;
+    console.log(this.form.getRawValue());
+  }
+}
