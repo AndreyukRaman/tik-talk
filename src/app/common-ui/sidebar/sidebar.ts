00555c7d4627347ba3e472a78885c8e46812c715
Raman Andreyuk <romaandreyuk@gmail.com>
2026-07-16 21:55:19 +0200

    add subscribers short list to sidebar

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SvgIcon } from '../svg-icon/svg-icon';
 import { SubscriberCard } from './subscriber-card/subscriber-card';
+import { ProfileService } from '../../data/services/profile.service';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIcon, RouterLink, RouterLinkActive, SubscriberCard],
+  imports: [SvgIcon, RouterLink, RouterLinkActive, SubscriberCard, AsyncPipe, JsonPipe],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  private profileService = inject(ProfileService);
+
+  subscribers$ = this.profileService.getSubscribersShortList();
+
+
   menuItems = [
     { label: 'Home', icon: 'home', link: '' },
     { label: 'Chats', icon: 'chats', link: 'chats' },
--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -2,6 +2,8 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Profile } from '../interfaces/profile.interface';
 import { environment } from '../../../environments/environment';
+import { Pageble } from '../interfaces/pageble.interface';
+import { map } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class ProfileService {
@@ -11,6 +13,15 @@ export class ProfileService {
   getTestAccounts() {
     return this.http.get<Profile[]>(`${this.baseApiUrl}/account/test_accounts`)
   }
+
+
+
+  getSubscribersShortList(){
+    return this.http.get<Pageble<Profile>>(`${this.baseApiUrl}/account/subscribers/`).pipe(
+      map(res => res.items.slice(0,3))
+    )
+  }
+
 
   getMe(){
     return this.http.get<Profile>(`${this.baseApiUrl}/account/me`)
