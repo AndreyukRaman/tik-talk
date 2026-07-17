88f0d8e0e19406c237bef7d8bc2e3dba5a5fbf2d
Raman Andreyuk <romaandreyuk@gmail.com>
2026-07-17 21:02:33 +0200

    use BehaviorSubject to guard concurrent token refresh

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,9 +1,9 @@
 import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
-import { catchError, switchMap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, filter, switchMap, take, throwError } from 'rxjs';
 
-let isRefreshing = false;
+const isRefreshing$ = new BehaviorSubject<boolean>(false);
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
@@ -26,18 +26,22 @@ const refreshAndProceed = (
   req: HttpRequest<any>,
   next: HttpHandlerFn
 ) => {
-  if (!isRefreshing) {
-    isRefreshing = true;
+  if (!isRefreshing$.value) {
+    isRefreshing$.next(true);
 
     return authService.refreshAuthToken().pipe(
       switchMap((res) => {
-        isRefreshing = false;
+        isRefreshing$.next(false);
         return next(addToken(req, res.access_token));
       })
     );
   }
 
-  return next(addToken(req, authService.token ?? ''));
+  return isRefreshing$.pipe(
+    filter((isRefreshing) => !isRefreshing),
+    take(1),
+    switchMap(() => next(addToken(req, authService.token ?? '')))
+  );
 };
 
 const addToken = (req: HttpRequest<any>, token: string) => {
